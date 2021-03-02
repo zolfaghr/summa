fb6e053ccf0d65b6d62664fcbddda95e82aad897
Reza Zolfaghari <zolfaghr@mcmaster.ca>
2021-03-02 00:06:46 -0600

    summa_data

---
+++ build/derivedTypeStruct/summa_data.h
@@ -0,0 +1,82 @@
+
+#ifndef SUMMA_DATA_H_
+#define SUMMA_DATA_H_
+
+#include <vector>
+#include <iostream>
+
+extern "C" void* new_handle_var_i();
+extern "C" void  delete_handle_var_i(void* handle);
+extern "C" void  set_data_var_i(void* handle, const int* array, int size);
+extern "C" void  get_size_data_var_i(void* handle, int* size);
+extern "C" void  get_data_var_i(void* handle, int* array);
+
+
+extern "C" void* new_handle_var_d();
+extern "C" void  delete_handle_var_d(void* handle);
+extern "C" void  set_data_var_d(void* handle, const double* array, int size);
+extern "C" void  get_size_data_var_d(void* handle, int* size);
+extern "C" void  get_data_var_d(void* handle, double* array);
+
+
+
+
+class summa_data  {
+private:
+  void *handle_var_i;
+  void *handle_var_d;
+public:
+  // constructors
+  summa_data()  { 
+  	handle_var_i = new_handle_var_i();
+  	handle_var_d = new_handle_var_d();
+  }
+  
+  summa_data(const std::vector<int> &arr_i, const std::vector<double> &arr_d) {
+  	handle_var_i = new_handle_var_i();
+    ::set_data_var_i(handle_var_i, &arr_i[0], arr_i.size());
+
+  	handle_var_d = new_handle_var_d();
+    ::set_data_var_d(handle_var_d, &arr_d[0], arr_d.size());
+  }
+  
+  
+  // methods
+  void set_data(const std::vector<int>& arr_i, const std::vector<double> &arr_d) {
+       ::set_data_var_i(handle_var_i, &arr_i[0], arr_i.size());
+       ::set_data_var_d(handle_var_d, &arr_d[0], arr_d.size());
+  }
+
+  std::vector<int> get_data_var_i() {
+    int size;
+    ::get_size_data_var_i(handle_var_i, &size);
+    if (size == 0) return std::vector<int>();
+
+    std::vector<int> array(size);
+    ::get_data_var_i(handle_var_i, &array[0]);
+    return array;
+  }
+  
+  std::vector<double> get_data_var_d() {
+    int size;
+    ::get_size_data_var_d(handle_var_d, &size);
+    if (size == 0) return std::vector<double>();
+
+    std::vector<double> array(size);
+    ::get_data_var_d(handle_var_d, &array[0]);
+    return array;
+  }
+  
+   void update() {
+    ::update_var_i(handle_var_i);
+    ::update_var_d(handle_var_d);
+   }
+  
+  // destructor 
+  ~summa_data() { 
+  	delete_handle_var_i(handle_var_i);
+  	delete_handle_var_d(handle_var_d);
+   }
+};
+
+#endif
--- build/derivedTypeStruct/test.cc
+++ build/derivedTypeStruct/test.cc
@@ -2,10 +2,11 @@
 #include <vector>
 #include "var_i.h"
 #include "var_d.h"
+#include "summa_data.h"
 
 int main()  {
   
-
+ // test var_i ************************************************
   std::cout << "-------test var_i--------" << std::endl;
   std::vector<int> vec_in{2,5,-3,0}, vec_out;
   var_i x(vec_in);
@@ -29,6 +30,8 @@ int main()  {
   	std::cout << vec_out[i] << "   ";
   std::cout << std::endl;
   
+  
+ // test var_d ************************************************ 
   std::cout << "-------test var_d--------" << std::endl;
   std::vector<double> vec_in_d{2.4,5.12,-3.1,0.8}, vec_out_d;
   var_d y(vec_in_d);
@@ -51,6 +54,8 @@ int main()  {
   for(size_t i=0; i<vec_out_d.size(); i++)
   	std::cout << vec_out_d[i] << "   ";
   std::cout << std::endl;
+  
+  // test summa_data ************************************************
 
   return 0;
 }
