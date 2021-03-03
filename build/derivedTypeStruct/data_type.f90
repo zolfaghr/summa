1b6a2bf83527e0e7d979428f9c5261b506168b34
Reza Zolfaghari <zolfaghr@mcmaster.ca>
2021-03-03 11:20:40 -0600

    data_type with nrtype

--- build/derivedTypeStruct/data_type.f90
+++ build/derivedTypeStruct/data_type.f90
@@ -1,18 +1,19 @@
 
 module data_type
+use nrtype
 
   implicit none
   
   type, public :: var_i
-    integer, allocatable :: var(:)
+    integer(i4b), allocatable :: var(:)
   end type var_i
   
   type, public :: var_d
-    real, allocatable :: var(:)
+    real(dp), allocatable :: var(:)
   end type var_d
   
   type, public :: dlength
-  	real, allocatable   :: dat(:)   
+  	real(dp), allocatable   :: dat(:)   
   end type dlength
   
   type, public :: var_dlength
