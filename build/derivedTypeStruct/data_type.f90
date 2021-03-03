2819873ab284ab0847185159e57966bd93d66f3f
Reza Zolfaghari <zolfaghr@mcmaster.ca>
2021-03-03 16:45:48 -0600

    var_ilength

--- build/derivedTypeStruct/data_type.f90
+++ build/derivedTypeStruct/data_type.f90
@@ -9,7 +9,7 @@ use nrtype
   end type var_i
   
  type, public :: var_i8
-  integer(8),allocatable              :: var(:) 
+  integer(8), allocatable              :: var(:) 
  endtype var_i8
   
   type, public :: var_d
@@ -21,13 +21,17 @@ use nrtype
  end type ilength
  
  type, public :: i8length
-  integer(8),allocatable              :: dat(:)    
+  integer(8), allocatable              :: dat(:)    
  end type i8length
    
   type, public :: dlength
   	real(dp), allocatable   :: dat(:)   
   end type dlength
-  
+
+ type, public :: var_ilength
+  type(ilength), allocatable           :: var(:)  
+ end type var_ilength
+   
   type, public :: var_dlength
   	type(dlength), allocatable	 :: var(:)
   end type var_dlength
---
+++ build/derivedTypeStruct/wrapper_var_ilength.f90
@@ -0,0 +1,124 @@
+
+module wrapper_var_ilength
+
+use, intrinsic :: iso_c_binding
+use data_type
+
+implicit none
+  
+! pointer-to-opaque-handle technique
+  
+contains
+
+!**************************************************
+! get C address of the object
+  function new_handle_var_ilength() result(handle) bind(C, name='new_handle_var_ilength')
+    
+    type(c_ptr) :: handle
+    type(var_ilength), pointer :: p
+    
+    allocate(p)    
+    handle = c_loc(p)   
+    
+  end function new_handle_var_ilength
+
+!**************************************************
+  subroutine delete_handle_var_ilength(handle) bind(C, name='delete_handle_var_ilength')
+    
+    type(c_ptr), intent(in), value :: handle
+    type(var_ilength), pointer :: p
+    
+    call c_f_pointer(handle, p)
+    deallocate(p)
+    
+  end subroutine delete_handle_var_ilength
+
+!**************************************************
+  subroutine set_data_var_ilength(handle, array, num_row, num_col, num_elements) bind(C, name='set_data_var_ilength')
+    
+    type(c_ptr), intent(in), value    :: handle
+    integer(c_int), intent(in), value :: num_row
+    integer(c_int), intent(in), value :: num_elements
+    integer(c_int), intent(in)   	  :: num_col(num_row) 
+    integer(c_int), intent(in)        :: array(num_elements)
+    type(var_ilength), pointer :: p
+    integer(c_int)  :: i,j,sum_elem
+    
+   
+    call c_f_pointer(handle, p)    
+    if (allocated(p%var)) then
+      if (size(p%var) /= num_row) then
+        deallocate(p%var)
+        allocate(p%var(num_row))
+        do i=1,num_row
+        	allocate( p%var(i)%dat(num_col(i)) )
+        end do
+      end if
+    else
+        allocate(p%var(num_row))
+        do i=1,num_row
+        	allocate( p%var(i)%dat(num_col(i)) )
+        end do
+    end if
+    
+    sum_elem = 0
+    do i=1,num_row
+    	do j=1,num_col(i)
+    		p%var(i)%dat(j) = array(sum_elem + j)
+    	end do
+    	sum_elem = sum_elem + num_col(i)
+    end do
+    
+  end subroutine set_data_var_ilength
+  
+!**************************************************
+  subroutine get_size_data_var_ilength(handle, var_size, dat_size) bind(C, name='get_size_data_var_ilength')
+    
+    type(c_ptr), intent(in), value :: handle
+    integer(c_int), intent(out) :: var_size
+    integer(c_int), intent(out) :: dat_size(*)
+    type(var_ilength), pointer :: p
+    integer(c_int)  :: i,j
+    
+    call c_f_pointer(handle, p)
+    if (allocated(p%var)) then
+      var_size = size(p%var, kind=c_int)
+      do i=1,var_size
+      	dat_size(i) = size(p%var(i)%dat, kind=c_int)
+      end do
+    else
+      var_size = 0_c_int
+    end if
+    
+  end subroutine get_size_data_var_ilength
+  
+!**************************************************
+  subroutine get_data_var_ilength(handle, array) bind(C, name='get_data_var_ilength')
+    
+    type(c_ptr), intent(in), value :: handle
+    integer(c_int), intent(out) :: array(*)
+    type(var_ilength), pointer :: p
+    integer(c_int)  :: i,j,size_var,size_dat,size_array
+    
+    call c_f_pointer(handle, p)
+    
+    size_array = 0
+    if (allocated(p%var)) then
+      size_var = size(p%var)
+      do i=1,size_var
+      	size_dat = size(p%var(i)%dat)
+      	do j=1,size_dat
+      		array(size_array+j) = p%var(i)%dat(j)
+      	end do
+      	size_array = size_array + size_dat
+      end do
+      
+    end if
+    
+  end subroutine get_data_var_ilength
+
+!=====================================================================
+
+end module wrapper_var_ilength
+
+
