aacb9902e96e8e157cb23783f2f47ea133a03de2
Reza Zolfaghari <zolfaghr@mcmaster.ca>
2021-03-16 21:56:53 -0600

    cppwrap_metadata created

---
+++ build/source/cppwrap/cppwrap_metadata.f90
@@ -0,0 +1,99 @@
+
+module cppwrap_metadata
+
+use, intrinsic :: iso_c_binding
+use data_types
+
+implicit none
+  
+contains
+
+! ***************** Auxiliary functions and subroutines ***********************
+
+  function comp_len_string(c_str) result(length)
+  	character(kind=c_char,len=1),intent(in)		:: c_str(*)
+  	integer 									:: length
+  	
+  	length=0
+    do
+       if (c_str(length+1) == C_NULL_CHAR) exit
+       length = length + 1
+    end do 
+    
+  end function comp_len_string
+  
+! -----------------------------------------------------------  	
+  subroutine c_f_string(c_str, f_str, l)
+  
+  	character(kind=c_char,len=1),intent(in)		:: c_str(*)
+  	integer, intent(in)							:: l
+  	character(len=l),intent(out)				:: f_str
+  
+  	character(len=:), allocatable 				:: temp
+  	integer 									:: length   
+      
+    length = comp_len_string(c_str)
+    
+    allocate(character(len=length) :: temp)    
+    temp = transfer(c_str(1:length), temp)
+    
+    f_str = temp
+    
+    deallocate(temp)
+    
+  end subroutine c_f_string
+  
+
+! **************************** var_info ********************************
+
+  function new_handle_var_info() result(handle) bind(C, name='new_handle_var_info')
+    
+    type(c_ptr) 			:: handle
+    type(var_info), pointer :: p
+    
+    allocate(p)    
+    handle = c_loc(p)   
+    
+  end function new_handle_var_info
+
+!-----------------------------------
+  subroutine delete_handle_var_info(handle) bind(C, name='delete_handle_var_info')
+    
+    type(c_ptr), intent(in), value :: handle
+    type(var_info), pointer :: p
+    
+    call c_f_pointer(handle, p)
+    deallocate(p)
+    
+  end subroutine delete_handle_var_info
+  
+!-----------------------------------
+  subroutine set_data_var_info(handle, varname, vartype, varDesire) bind(C, name='set_data_var_info')
+    
+    type(c_ptr), intent(in), 	value 			:: handle
+  	character(kind=c_char,len=1),intent(in)		:: varname(*)
+  	integer(c_int),intent(in),  value       	:: vartype   
+  	integer(c_int),intent(in),  value       	:: varDesire
+  	
+    type(var_info), pointer 					:: p
+    character(len=:), allocatable 				:: temp
+	    
+    call c_f_pointer(handle, p)
+    
+    call c_f_string(varname, p%varname, 64)
+    p%vartype = vartype
+    if(varDesire == 0)then; p%varDesire = .false.; else; p%varDesire = .true.; endif
+    
+    print *, p%varname
+    print *, p%vartype
+    print *, p%varDesire
+    
+
+    
+  end subroutine set_data_var_info
+
+
+     
+end module cppwrap_metadata
+
+
